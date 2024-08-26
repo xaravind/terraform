abf3663c0de83526e74be091484cfcb7ddd96fa7
xaravind <aravindbasava1@gmail.com>
2024-08-26 14:58:06 +0530

    terraform

---
+++ for-each/data.tf
@@ -0,0 +1,21 @@
+
+data "aws_ami" "ami_info" {
+
+    most_recent = true
+    owners = ["973714476881"]
+
+    filter {
+        name   = "name"
+        values = ["RHEL-9-DevOps-Practice"]
+    }
+
+    filter {
+        name   = "root-device-type"
+        values = ["ebs"]
+    }
+
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+}
